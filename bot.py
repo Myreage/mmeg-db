0d696dcbc8e56c0318af483de66d4533eabd4ae2
Benjamin Michotte <bmichotte@gmail.com>
2018-07-19 11:32:39 +0200

    retourne un message d’erreur si rien trouvé

--- bot.py
+++ bot.py
@@ -17,17 +17,20 @@ async def on_ready():
 @bot.command(pass_context=True, name='creature')
 async def creature(context, *args):
 
-    res = showCreature(' '.join(args))   
-    embed = discord.Embed(title=res['title'], color=0x00ff00)
-    embed.add_field(name="__**Informations**__", value=res['head'], inline=True)
-    embed.add_field(name="__**Caractéristiques**__", value=res['stats'], inline=True)
-    embed.add_field(name="__**Lien vers la fiche**__", value=res['provider'], inline=True)
-    embed.add_field(name="__**Sorts**__", value=res['spells'], inline=False)      
-    embed.set_thumbnail(url=res['image'])
-    embed.set_footer(text="Données fournies par mmeg-db.com")
+    res = showCreature(' '.join(args))
+    if (res == False):
+        await bot.send_message(context.message.channel, 'Je ne trouve pas' + ' '.join(args))
+    else:
+        embed = discord.Embed(title=res['title'], color=0x00ff00)
+        embed.add_field(name="__**Informations**__", value=res['head'], inline=True)
+        embed.add_field(name="__**Caractéristiques**__", value=res['stats'], inline=True)
+        embed.add_field(name="__**Lien vers la fiche**__", value=res['provider'], inline=True)
+        embed.add_field(name="__**Sorts**__", value=res['spells'], inline=False)      
+        embed.set_thumbnail(url=res['image'])
+        embed.set_footer(text="Données fournies par mmeg-db.com")
    
        
-    await bot.send_message(context.message.channel, embed=embed)
+        await bot.send_message(context.message.channel, embed=embed)
 
 bot.run(TOKEN)
 
